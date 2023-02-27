60a73cec1866de3224a600ebcbd5d65b71a3bd5d
tanyavm7 <90417982+tanyavm7@users.noreply.github.com>
2023-02-27 13:42:38 -0500

    Update gradebook.h

--- gradebook.h
+++ gradebook.h
@@ -14,17 +14,14 @@ private:
 
 public:
     Gradebook();
-    void calcTotalPossible();
-    void calcTotalGrade();
+    float calcTotalPossible();
+    float calcTotalGrade();
     void inFile(std::string fileName);
     void outFile(std::string fileName);
     void getOneGrade(std::string category, int assignmentNum);
     void getCategoryGrade(std::string category);
     void addGrade(std::string Category, int index,std::string Grade);
     void changeGrade(std::string Category, int index, std::string Grade);
-
-
-
 
 };
 
